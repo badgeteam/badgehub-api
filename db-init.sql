086f8df4b0eae80d8d0425fd1fd176a8c994e7be
Edwin Martin <edwin@bitstorm.org>
2024-04-20 14:19:20 +0200

    Add Docker compose, Node.js and Postgresql

---
+++ db-init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE public.test (
+  name character varying(255) NOT NULL
+);
+insert into "public"."test" ("name") values ('hello');
+insert into "public"."test" ("name") values ('world');
