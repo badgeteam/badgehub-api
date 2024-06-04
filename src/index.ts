8fe18ed81b8af263fe52b707b6eb90883812c0f0
Edwin Martin <edwin@bitstorm.org>
2024-06-04 16:04:21 +0200

    Add logging

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { RegisterRoutes } from "./generated/routes.js";
 import openapi from "./openapi.js";
+import { pinoHttp } from "pino-http";
 
 const app = express();
 const port = 8081;
+const pino = pinoHttp();
 
 app.use(express.json());
 app.use(express.static("public"));
+app.use(pino);
 
 app.listen(port, () => {
   console.info(`Node.js server started.`);
