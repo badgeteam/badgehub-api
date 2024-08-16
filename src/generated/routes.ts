ef29b5618ba2b133324175130ebac444df7daa4e
Edwin Martin <edwin@bitstorm.org>
2024-08-16 12:22:03 +0200

    Fix "categrory_slug" and some minor changes

--- src/generated/routes.ts
+++ src/generated/routes.ts
@@ -1,195 +1,252 @@
 /* tslint:disable */
 /* eslint-disable */
 // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-import { TsoaRoute, fetchMiddlewares, ExpressTemplateService } from '@tsoa/runtime';
+import {
+  TsoaRoute,
+  fetchMiddlewares,
+  ExpressTemplateService,
+} from "@tsoa/runtime";
 // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-import { RestController } from './../public-rest.js';
-import type { Request as ExRequest, Response as ExResponse, RequestHandler, Router } from 'express';
-
-
+import { RestController } from "./../public-rest.js";
+import type {
+  Request as ExRequest,
+  Response as ExResponse,
+  RequestHandler,
+  Router,
+} from "express";
 
 // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 
 const models: TsoaRoute.Models = {
-    "Device": {
-        "dataType": "refObject",
-        "properties": {
-            "name": {"dataType":"string","required":true},
-            "slug": {"dataType":"string","required":true},
-        },
-        "additionalProperties": false,
+  Device: {
+    dataType: "refObject",
+    properties: {
+      name: { dataType: "string", required: true },
+      slug: { dataType: "string", required: true },
     },
-    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-    "Category": {
-        "dataType": "refObject",
-        "properties": {
-            "name": {"dataType":"string","required":true},
-            "slug": {"dataType":"string","required":true},
-        },
-        "additionalProperties": false,
+    additionalProperties: false,
+  },
+  // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+  Category: {
+    dataType: "refObject",
+    properties: {
+      name: { dataType: "string", required: true },
+      slug: { dataType: "string", required: true },
     },
-    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-    "App": {
-        "dataType": "refObject",
-        "properties": {
-            "name": {"dataType":"string","required":true},
-            "slug": {"dataType":"string","required":true},
-            "categrory_slug": {"dataType":"string","required":true},
-            "user_name": {"dataType":"string","required":true},
-        },
-        "additionalProperties": false,
+    additionalProperties: false,
+  },
+  // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+  App: {
+    dataType: "refObject",
+    properties: {
+      name: { dataType: "string", required: true },
+      slug: { dataType: "string", required: true },
+      category_slug: { dataType: "string", required: true },
+      user_name: { dataType: "string", required: true },
     },
-    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-    "AppDetails": {
-        "dataType": "refObject",
-        "properties": {
-            "name": {"dataType":"string","required":true},
-            "slug": {"dataType":"string","required":true},
-            "description": {"dataType":"string","required":true},
-            "categrory_slug": {"dataType":"string","required":true},
-            "user_name": {"dataType":"string","required":true},
-            "devices": {"dataType":"array","array":{"dataType":"string"},"required":true},
-        },
-        "additionalProperties": false,
+    additionalProperties: false,
+  },
+  // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+  AppDetails: {
+    dataType: "refObject",
+    properties: {
+      name: { dataType: "string", required: true },
+      slug: { dataType: "string", required: true },
+      description: { dataType: "string", required: true },
+      category_slug: { dataType: "string", required: true },
+      user_name: { dataType: "string", required: true },
+      devices: {
+        dataType: "array",
+        array: { dataType: "string" },
+        required: true,
+      },
     },
-    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    additionalProperties: false,
+  },
+  // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 };
-const templateService = new ExpressTemplateService(models, {"noImplicitAdditionalProperties":"throw-on-extras","bodyCoercion":true});
+const templateService = new ExpressTemplateService(models, {
+  noImplicitAdditionalProperties: "throw-on-extras",
+  bodyCoercion: true,
+});
 
 // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 
 export function RegisterRoutes(app: Router) {
-    // ###########################################################################################################
-    //  NOTE: If you do not see routes for all of your controllers in this file, then you might not have informed tsoa of where to look
-    //      Please look into the "controllerPathGlobs" config option described in the readme: https://github.com/lukeautry/tsoa
-    // ###########################################################################################################
-        app.get('/api/v3/devices',
-            ...(fetchMiddlewares<RequestHandler>(RestController)),
-            ...(fetchMiddlewares<RequestHandler>(RestController.prototype.getDevices)),
+  // ###########################################################################################################
+  //  NOTE: If you do not see routes for all of your controllers in this file, then you might not have informed tsoa of where to look
+  //      Please look into the "controllerPathGlobs" config option described in the readme: https://github.com/lukeautry/tsoa
+  // ###########################################################################################################
+  app.get(
+    "/api/v3/devices",
+    ...fetchMiddlewares<RequestHandler>(RestController),
+    ...fetchMiddlewares<RequestHandler>(RestController.prototype.getDevices),
 
-            async function RestController_getDevices(request: ExRequest, response: ExResponse, next: any) {
-            const args: Record<string, TsoaRoute.ParameterSchema> = {
-            };
+    async function RestController_getDevices(
+      request: ExRequest,
+      response: ExResponse,
+      next: any
+    ) {
+      const args: Record<string, TsoaRoute.ParameterSchema> = {};
 
-            // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+      // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 
-            let validatedArgs: any[] = [];
-            try {
-                validatedArgs = templateService.getValidatedArgs({ args, request, response });
-
-                const controller = new RestController();
-
-              await templateService.apiHandler({
-                methodName: 'getDevices',
-                controller,
-                response,
-                next,
-                validatedArgs,
-                successStatus: undefined,
-              });
-            } catch (err) {
-                return next(err);
-            }
+      let validatedArgs: any[] = [];
+      try {
+        validatedArgs = templateService.getValidatedArgs({
+          args,
+          request,
+          response,
         });
-        // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-        app.get('/api/v3/categories',
-            ...(fetchMiddlewares<RequestHandler>(RestController)),
-            ...(fetchMiddlewares<RequestHandler>(RestController.prototype.getCategories)),
 
-            async function RestController_getCategories(request: ExRequest, response: ExResponse, next: any) {
-            const args: Record<string, TsoaRoute.ParameterSchema> = {
-            };
+        const controller = new RestController();
 
-            // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-
-            let validatedArgs: any[] = [];
-            try {
-                validatedArgs = templateService.getValidatedArgs({ args, request, response });
-
-                const controller = new RestController();
-
-              await templateService.apiHandler({
-                methodName: 'getCategories',
-                controller,
-                response,
-                next,
-                validatedArgs,
-                successStatus: undefined,
-              });
-            } catch (err) {
-                return next(err);
-            }
+        await templateService.apiHandler({
+          methodName: "getDevices",
+          controller,
+          response,
+          next,
+          validatedArgs,
+          successStatus: undefined,
         });
-        // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-        app.get('/api/v3/apps',
-            ...(fetchMiddlewares<RequestHandler>(RestController)),
-            ...(fetchMiddlewares<RequestHandler>(RestController.prototype.getApps)),
+      } catch (err) {
+        return next(err);
+      }
+    }
+  );
+  // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+  app.get(
+    "/api/v3/categories",
+    ...fetchMiddlewares<RequestHandler>(RestController),
+    ...fetchMiddlewares<RequestHandler>(RestController.prototype.getCategories),
 
-            async function RestController_getApps(request: ExRequest, response: ExResponse, next: any) {
-            const args: Record<string, TsoaRoute.ParameterSchema> = {
-                    pageStart: {"in":"query","name":"pageStart","dataType":"double"},
-                    pageLength: {"in":"query","name":"pageLength","dataType":"double"},
-                    category: {"in":"query","name":"category","dataType":"string"},
-                    device: {"in":"query","name":"device","dataType":"string"},
-            };
+    async function RestController_getCategories(
+      request: ExRequest,
+      response: ExResponse,
+      next: any
+    ) {
+      const args: Record<string, TsoaRoute.ParameterSchema> = {};
 
-            // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+      // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 
-            let validatedArgs: any[] = [];
-            try {
-                validatedArgs = templateService.getValidatedArgs({ args, request, response });
-
-                const controller = new RestController();
-
-              await templateService.apiHandler({
-                methodName: 'getApps',
-                controller,
-                response,
-                next,
-                validatedArgs,
-                successStatus: undefined,
-              });
-            } catch (err) {
-                return next(err);
-            }
+      let validatedArgs: any[] = [];
+      try {
+        validatedArgs = templateService.getValidatedArgs({
+          args,
+          request,
+          response,
         });
-        // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-        app.get('/api/v3/apps/:slug',
-            ...(fetchMiddlewares<RequestHandler>(RestController)),
-            ...(fetchMiddlewares<RequestHandler>(RestController.prototype.getAppDetails)),
 
-            async function RestController_getAppDetails(request: ExRequest, response: ExResponse, next: any) {
-            const args: Record<string, TsoaRoute.ParameterSchema> = {
-                    slug: {"in":"path","name":"slug","required":true,"dataType":"string"},
-                    notFoundResponse: {"in":"res","name":"404","required":true,"dataType":"nestedObjectLiteral","nestedProperties":{"reason":{"dataType":"string","required":true}}},
-            };
+        const controller = new RestController();
 
-            // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
-
-            let validatedArgs: any[] = [];
-            try {
-                validatedArgs = templateService.getValidatedArgs({ args, request, response });
-
-                const controller = new RestController();
-
-              await templateService.apiHandler({
-                methodName: 'getAppDetails',
-                controller,
-                response,
-                next,
-                validatedArgs,
-                successStatus: undefined,
-              });
-            } catch (err) {
-                return next(err);
-            }
+        await templateService.apiHandler({
+          methodName: "getCategories",
+          controller,
+          response,
+          next,
+          validatedArgs,
+          successStatus: undefined,
         });
-        // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+      } catch (err) {
+        return next(err);
+      }
+    }
+  );
+  // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+  app.get(
+    "/api/v3/apps",
+    ...fetchMiddlewares<RequestHandler>(RestController),
+    ...fetchMiddlewares<RequestHandler>(RestController.prototype.getApps),
 
-    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    async function RestController_getApps(
+      request: ExRequest,
+      response: ExResponse,
+      next: any
+    ) {
+      const args: Record<string, TsoaRoute.ParameterSchema> = {
+        pageStart: { in: "query", name: "pageStart", dataType: "double" },
+        pageLength: { in: "query", name: "pageLength", dataType: "double" },
+        category: { in: "query", name: "category", dataType: "string" },
+        device: { in: "query", name: "device", dataType: "string" },
+      };
 
+      // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 
-    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+      let validatedArgs: any[] = [];
+      try {
+        validatedArgs = templateService.getValidatedArgs({
+          args,
+          request,
+          response,
+        });
+
+        const controller = new RestController();
+
+        await templateService.apiHandler({
+          methodName: "getApps",
+          controller,
+          response,
+          next,
+          validatedArgs,
+          successStatus: undefined,
+        });
+      } catch (err) {
+        return next(err);
+      }
+    }
+  );
+  // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+  app.get(
+    "/api/v3/apps/:slug",
+    ...fetchMiddlewares<RequestHandler>(RestController),
+    ...fetchMiddlewares<RequestHandler>(RestController.prototype.getAppDetails),
+
+    async function RestController_getAppDetails(
+      request: ExRequest,
+      response: ExResponse,
+      next: any
+    ) {
+      const args: Record<string, TsoaRoute.ParameterSchema> = {
+        slug: { in: "path", name: "slug", required: true, dataType: "string" },
+        notFoundResponse: {
+          in: "res",
+          name: "404",
+          required: true,
+          dataType: "nestedObjectLiteral",
+          nestedProperties: { reason: { dataType: "string", required: true } },
+        },
+      };
+
+      // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+
+      let validatedArgs: any[] = [];
+      try {
+        validatedArgs = templateService.getValidatedArgs({
+          args,
+          request,
+          response,
+        });
+
+        const controller = new RestController();
+
+        await templateService.apiHandler({
+          methodName: "getAppDetails",
+          controller,
+          response,
+          next,
+          validatedArgs,
+          successStatus: undefined,
+        });
+      } catch (err) {
+        return next(err);
+      }
+    }
+  );
+  // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+
+  // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+
+  // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 }
 
 // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
