9bd171e929a71e8fa8c6b17d69374db4bfd4b01b
Francis Duvivier <francis.duvivier@persgroep.net>
2024-11-07 11:55:09 +0100

    Clean up relations in ...FromUser

--- src/domain/models/app/VoteFromUser.ts
+++ src/domain/models/app/VoteFromUser.ts
@@ -1,9 +1,6 @@
 import { UserRelation } from "./User";
 import { DatedData } from "./DatedData";
 import { Vote } from "@db/newModels/app/Vote";
-export interface VoteFromUserRelation {
-  vote_from_user_id: VoteFromUser["id"];
-}
 
 export interface VoteFromUser extends Vote, UserRelation, DatedData {
   id: number;
--- src/domain/models/app/WarningFromUser.ts
+++ src/domain/models/app/WarningFromUser.ts
@@ -2,10 +2,6 @@ import { UserRelation } from "./User";
 import { DatedData } from "./DatedData";
 import { Warning } from "@db/newModels/app/Warning";
 
-export interface WarningFromUserRelation {
-  warning_from_user_id: WarningFromUser["id"];
-}
-
 export interface WarningFromUser extends Warning, UserRelation, DatedData {
   id: number;
 }
