cd6ad717c5a6cc27d9b36b83b257da791c790e01
Francis Duvivier <francis.duvivier@persgroep.net>
2024-11-07 12:41:02 +0100

    Add some more necessary items to ProjectPort.ts

--- src/domain/aggregates/ProjectPort.ts
+++ src/domain/aggregates/ProjectPort.ts
@@ -2,17 +2,26 @@ import { Project } from "@domain/models/app/Project";
 import { Version } from "@domain/models/app/Version";
 import { User } from "@domain/models/app/User";
 import { FileMetadata } from "@domain/models/app/FileMetadata";
-import { MetadataFileContents } from "@domain/models/app/MetadataFileContents";
+import {
+  AppCategory,
+  MetadataFileContents,
+} from "@domain/models/app/MetadataFileContents";
 import { ProjectSlug } from "@domain/models/app/Project";
+import { Badge } from "@domain/models/Badge";
 
 interface ProjectPort {
   publishUpdatedMetadata(changes: MetadataFileContents): Promise<void>;
   updateFile(filePath: string, contents: string): Promise<void>;
-  publishVersion(slug: ProjectSlug): Promise<void>; // Publishes the current state of the app as a version
-  getAppProject(slug: ProjectSlug): Promise<Project>;
-  getAppVersion(slug: ProjectSlug): Promise<Version>;
+  publishVersion(projectSlug: ProjectSlug): Promise<void>; // Publishes the current state of the app as a version
+  getAppProject(projectSlug: ProjectSlug): Promise<Project>;
+  getAppVersion(projectSlug: ProjectSlug): Promise<Version>;
   getUser(userId: User["id"]): Promise<User>;
   updateUser(updatedUser: User): Promise<void>;
   getFileDownloadLink(fileId: FileMetadata["id"]): Promise<string>;
   getVersionDownloadLink(versionId: Version["id"]): Promise<string>;
+  getBadges(): Promise<Badge>;
+  getProjects(filter?: {
+    badgeSlug?: Badge["slug"];
+    appCategory?: AppCategory;
+  }): Promise<Project[]>;
 }
