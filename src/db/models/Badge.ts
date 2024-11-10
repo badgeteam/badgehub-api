9b2a3334abae36eddfe6b4536eddf1d8dc2d0a39
Francis Duvivier <francis.duvivier@persgroep.net>
2024-11-10 12:15:22 +0100

    Clean up imports

--- src/db/models/Badge.ts
+++ src/db/models/Badge.ts
@@ -1,6 +1,4 @@
-import { ProjectStatusOnBadge } from "./ProjectStatusOnBadge";
 import { DatedData } from "./app/DatedData";
-import { Project } from "./app/Project";
 export type BadgeSlug = string;
 export interface BadgeRelation {
   badge_id: Badge["id"];
