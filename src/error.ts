c8da0bf844c71d67d6d8679962b231a23591783b
Pauline Vos <pv@daedalean.ai>
2024-11-13 14:35:45 +0100

    Create BadgehubApiError base
    For improved API error handling. If we create many more after this we
    can add more types based on the base error.

---
+++ src/error.ts
@@ -0,0 +1,42 @@
+enum ErrorType {
+  NotAuthorized = "ERROR_NOT_AUTHORIZED",
+  NotAuthenticated = "ERROR_NOT_AUTHENTICATED",
+}
+class BadgeHubApiError extends Error {
+  name: ErrorType;
+  message: string;
+  cause: any;
+
+  constructor({
+    name,
+    message,
+    cause,
+  }: {
+    name: ErrorType;
+    message: string;
+    cause?: any;
+  }) {
+    super();
+    this.name = name;
+    this.message = message;
+    this.cause = cause;
+  }
+}
+const NotAuthenticatedError = (
+  message: string = "You are not authenticated"
+): BadgeHubApiError => {
+  return new BadgeHubApiError({ name: ErrorType.NotAuthenticated, message });
+};
+
+const NotAuthorizedError = (
+  message: string = "You are not authorized"
+): BadgeHubApiError => {
+  return new BadgeHubApiError({ name: ErrorType.NotAuthorized, message });
+};
+
+export {
+  ErrorType,
+  BadgeHubApiError,
+  NotAuthorizedError,
+  NotAuthenticatedError,
+};
