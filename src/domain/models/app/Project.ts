30c3998385242bb1738c6de09e427f216db7dba3
Francis Duvivier <francis.duvivier@persgroep.net>
2024-11-13 02:58:38 +0100

    Clean up unused ProjectRelation

--- src/domain/models/app/Project.ts
+++ src/domain/models/app/Project.ts
@@ -56,7 +56,3 @@ interface Dependency {
   // Changed! semantic_version_range added
   semantic_version_range: string; // Semantic version range specification that allows tilde, caret, wildcard specification of the version of a project that should be used. Following what is described here: https://python-poetry.org/docs/dependency-specification/
 }
-
-export interface ProjectRelation {
-  project: Project;
-}
