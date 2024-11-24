0268ee53d14f471513ac7a80a421178af0cf71b0
Francis Duvivier <francis.duvivier@persgroep.net>
2024-11-24 13:34:43 +0100

    WIP Add data migration to initialize-up.sql scripts

--- migrations/sqls/20241116085102-initialize-up.sql
+++ migrations/sqls/20241116085102-initialize-up.sql
@@ -14,7 +14,7 @@ create table badges
     name       text,
     created_at timestamptz default now(), -- creation timestamp
     updated_at timestamptz default now(), -- update timestamp
-    deleted_at timestamptz   -- soft delete timestamp (nullable)
+    deleted_at timestamptz                -- soft delete timestamp (nullable)
 );
 
 create table users
@@ -28,17 +28,17 @@ create table users
     editor            text,
     public            boolean,
     show_projects     boolean,
-    email_verified_at timestamptz, -- email verification timestamp
+    email_verified_at timestamptz,               -- email verification timestamp
     created_at        timestamptz default now(), -- to capture creation timestamps
     updated_at        timestamptz default now(), -- to track updates
-    deleted_at        timestamptz   -- soft delete timestamp (nullable)
+    deleted_at        timestamptz                -- soft delete timestamp (nullable)
 );
 
 create table projects
 (
     created_at       timestamptz not null default now(),
     updated_at       timestamptz not null default now(),
-    deleted_at       timestamptz         ,
+    deleted_at       timestamptz,
     version_id       integer,
     user_id          text        not null,
     slug             text        not null primary key,
@@ -53,10 +53,10 @@ create index idx_user_id on projects (user_id); -- allow searching projects by u
 create table categories
 (
     slug       text not null primary key, -- category slug
-    name       text not null, -- category name
+    name       text not null,             -- category name
     created_at timestamptz default now(), -- creation timestamp
     updated_at timestamptz default now(), -- update timestamp
-    deleted_at timestamptz   -- soft delete timestamp (nullable)
+    deleted_at timestamptz                -- soft delete timestamp (nullable)
 );
 
 -- index for faster lookups and soft delete queries
@@ -73,8 +73,8 @@ create table app_metadata_jsons
     author                    text,                        -- optional user_name name
     icon                      text,                        -- optional relative path for the icon
     license_file              text,                        -- optional license file path or type name
-    is_library                boolean     default false,   -- whether the app is a library
-    is_hidden                 boolean     default false,   -- whether the app is hidden in the launcher
+    is_library                boolean,   -- whether the app is a library
+    is_hidden                 boolean,   -- whether the app is hidden in the launcher
     semantic_version          text,                        -- semantic version
     interpreter               text,                        -- interpreter (e.g., 'python')
     main_executable           text,                        -- main executable path
@@ -83,7 +83,7 @@ create table app_metadata_jsons
     file_mappings_overrides   jsonb,                       -- overrides or additions for file mappings
     created_at                timestamptz default now(),   -- record creation timestamp
     updated_at                timestamptz default now(),   -- record update timestamp
-    deleted_at                timestamptz,    -- soft delete timestamp (nullable)
+    deleted_at                timestamptz,                 -- soft delete timestamp (nullable)
     constraint app_metadata_jsons_category_fk foreign key (category)
         references categories (slug) on delete set default -- category_slug relation
 );
@@ -95,19 +95,19 @@ create index idx_app_metadata_jsons_category on app_metadata_jsons (category);
 
 create table versions
 (
-    id                   serial primary key,        -- auto-incrementing unique identifier
-    project_slug         text    not null,          -- fk to projects
-    app_metadata_json_id integer not null,          -- fk to metadata files
-    revision             integer not null default 0,          -- version revision number
-    semantic_version     text,                      -- optional semantic version
-    zip                  text,                      -- optional zip file name or path
-    size_of_zip          bigint,                    -- optional size of the zip file
-    git_commit_id        text,                      -- optional git commit id
-    published_at         timestamptz,               -- required publish timestamp
-    download_count       bigint      default 0,     -- download count with default value
-    created_at           timestamptz default now(), -- track creation time
-    updated_at           timestamptz default now(), -- track updates
-    deleted_at           timestamptz,               -- soft delete timestamp (nullable)
+    id                   serial primary key,             -- auto-incrementing unique identifier
+    project_slug         text    not null,               -- fk to projects
+    app_metadata_json_id integer not null,               -- fk to metadata files
+    revision             integer not null default 0,     -- version revision number
+    semantic_version     text,                           -- optional semantic version
+    zip                  text,                           -- optional zip file name or path
+    size_of_zip          bigint,                         -- optional size of the zip file
+    git_commit_id        text,                           -- optional git commit id
+    published_at         timestamptz,                    -- required publish timestamp
+    download_count       bigint           default 0,     -- download count with default value
+    created_at           timestamptz      default now(), -- track creation time
+    updated_at           timestamptz      default now(), -- track updates
+    deleted_at           timestamptz,                    -- soft delete timestamp (nullable)
     constraint versions_project_slug_fk foreign key (project_slug) references projects (slug) on delete cascade,
     constraint versions_app_metadata_json_id_fk foreign key (app_metadata_json_id) references app_metadata_jsons (id) on delete cascade
 );
@@ -116,3 +116,20 @@ alter table projects
 
 create index idx_versions_project_slug on versions (project_slug);
 create index idx_versions_published_at on versions (published_at);
+
+-- Copy data from old schema to new schema
+INSERT INTO badgehub.badges (name, slug, deleted_at, created_at, updated_at)
+SELECT name, slug, deleted_at, created_at, updated_at
+FROM badgehub_old.badges;
+
+INSERT INTO badgehub.categories (name, slug, deleted_at, created_at, updated_at)
+SELECT name, slug, deleted_at, created_at, updated_at
+FROM badgehub_old.categories;
+
+INSERT INTO badgehub.users (id, admin, name, email, email_verified_at, password, remember_token, editor, public, show_projects, deleted_at, created_at, updated_at)
+SELECT id, admin, name, email, email_verified_at, password, remember_token, editor, public, show_projects, deleted_at, created_at, updated_at
+FROM badgehub_old.users;
+
+INSERT INTO badgehub.projects (slug, user_id, git, allow_team_fixes, deleted_at, created_at, updated_at)
+SELECT slug, user_id, git, allow_team_fixes, deleted_at, created_at, updated_at
+FROM badgehub_old.projects;
