e8ade1caf00b424b6985ed3cd71cd91307204a5c
Francis Duvivier <francisduvivier@gmail.com>
2024-11-27 19:57:18 +0100

    Improve logging in case of tsoa validation error

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import app from "./app";
 import { RegisterRoutes } from "./generated/routes";
+import { addTsoaValidationFailureLogging } from "@util/logging";
 import { EXPRESS_PORT } from "@config";
 
 async function startServer() {
   RegisterRoutes(app);
+
+  addTsoaValidationFailureLogging(app);
 
   app.listen(EXPRESS_PORT, () => {
     console.info(`Node.js server started.`);
