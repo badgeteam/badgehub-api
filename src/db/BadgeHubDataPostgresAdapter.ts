fdff70704a1868b20280aecd11f5f02e732abef2
Francis Duvivier <francisduvivier@gmail.com>
2024-11-27 22:03:37 +0100

    Extract objectToSQL.ts

--- src/db/BadgeHubDataPostgresAdapter.ts
+++ src/db/BadgeHubDataPostgresAdapter.ts
@@ -33,13 +33,7 @@ import {
   DBInsertAppMetadataJSON,
 } from "@db/models/app/DBAppMetadataJSON";
 import { DBCategory } from "@db/models/app/DBCategory";
-
-function getInsertKeysAndValuesSql(user: Object) {
-  const definedEntries = getEntriesWithDefinedValues(user);
-  const keys = join(definedEntries.map(([key]) => raw(key))); // raw is ok here because these keys are checked against our typescript definitions by tsoa
-  const values = join(definedEntries.map(([, value]) => value));
-  return { keys, values };
-}
+import { getInsertKeysAndValuesSql } from "@db/sqlHelpers/objectToSQL";
 
 function getUpdateAssigmentsSql(
   definedEntries: Entry<Partial<Omit<ProjectCore, "slug">>>[]
