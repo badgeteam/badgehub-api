7293c53d9b9097ff5f77361d65fd8cc349141a97
Francis Duvivier <francis.duvivier@persgroep.net>
2024-11-28 00:30:44 +0100

    Fix vite unit tests

--- src/controllers/public-rest.test.ts
+++ src/controllers/public-rest.test.ts
@@ -1,9 +1,11 @@
 import { describe, test, expect } from "vitest";
 import request from "supertest";
 import app from "../app";
+import { RegisterRoutes } from "@generated/routes";
 
 describe("API Routes", () => {
   test("GET /vitest", async () => {
+    RegisterRoutes(app);
     const res = await request(app).get("/api/v3/devices");
     expect(res.statusCode).toBe(200);
     expect(res.body).toHaveLength(3);
