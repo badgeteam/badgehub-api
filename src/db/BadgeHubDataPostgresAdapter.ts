ce5f890482304bc8bf807e07b178eb48ac3e099b
Francis Duvivier <francis.duvivier@persgroep.net>
2024-11-27 21:02:34 +0100

    Add write methods to domain and api and implement project and user insert

--- src/db/BadgeHubDataPostgresAdapter.ts
+++ src/db/BadgeHubDataPostgresAdapter.ts
@@ -1,12 +1,19 @@
 import { BadgeHubDataPort } from "@domain/BadgeHubDataPort";
 import { Badge } from "@domain/readModels/Badge";
-import { Project } from "@domain/readModels/app/Project";
+import {
+  Project,
+  ProjectCore,
+  ProjectSlug,
+} from "@domain/readModels/app/Project";
 import { User } from "@domain/readModels/app/User";
 import { Version } from "@domain/readModels/app/Version";
 import { Category } from "@domain/readModels/app/Category";
 import { Pool } from "pg";
 import { getPool } from "@db/connectionPool";
-import sql, { join } from "sql-template-tag";
+import { DBProject as DBProject } from "@db/models/app/DBProject";
+import sql, { join, raw } from "sql-template-tag";
+import { DBInsertUser } from "@db/models/app/DBUser";
+import { Entry, getEntriesWithDefinedValues } from "@util/objectEntries";
 import { DBBadge } from "@db/models/DBBadge";
 import {
   getBaseSelectProjectQuery,
@@ -21,14 +28,46 @@ import {
   timestampTZToDate,
 } from "@db/sqlHelpers/dbDates";
 import { DBVersion } from "@db/models/app/DBVersion";
-import { DBAppMetadataJSON } from "@db/models/app/DBAppMetadataJSON";
+import {
+  DBAppMetadataJSON,
+  DBInsertAppMetadataJSON,
+} from "@db/models/app/DBAppMetadataJSON";
 import { DBCategory } from "@db/models/app/DBCategory";
+
+function getInsertKeysAndValuesSql(user: Object) {
+  const definedEntries = getEntriesWithDefinedValues(user);
+  const keys = join(definedEntries.map(([key]) => raw(key))); // raw is ok here because these keys are checked against our typescript definitions by tsoa
+  const values = join(definedEntries.map(([, value]) => value));
+  return { keys, values };
+}
+
+function getUpdateAssigmentsSql(
+  definedEntries: Entry<Partial<Omit<ProjectCore, "slug">>>[]
+) {
+  return join(
+    definedEntries.map(
+      ([
+        key,
+        value,
+      ]) => sql`${raw(key)} // raw is ok here because these keys are checked against our typescript definitions by tsoa
+        =
+        ${value}`
+    )
+  );
+}
 
 export class BadgeHubDataPostgresAdapter implements BadgeHubDataPort {
   private readonly pool: Pool;
 
   constructor() {
     this.pool = getPool();
+  }
+
+  async insertUser(user: DBInsertUser): Promise<void> {
+    const { keys, values } = getInsertKeysAndValuesSql(user);
+    const insertQuery = sql`insert into users (${keys})
+                            values (${values})`;
+    await this.pool.query(insertQuery);
   }
 
   async getCategories(): Promise<Category[]> {
@@ -38,11 +77,76 @@ export class BadgeHubDataPostgresAdapter implements BadgeHubDataPort {
     return dbCategoryNames.map((dbCategory) => dbCategory);
   }
 
+  async insertProject(project: DBProject): Promise<void> {
+    const { keys, values } = getInsertKeysAndValuesSql(project);
+    const insertAppMetadataSql = sql`insert into app_metadata_jsons (name)
+                                     values (${project.slug})`;
+
+    await this.pool.query(sql`
+        with inserted_app_metadata as (${insertAppMetadataSql} returning id),
+             inserted_version as (
+                 insert
+                     into versions (project_slug, app_metadata_json_id)
+                         values (${project.slug}, (select id from inserted_app_metadata)) returning id)
+        insert
+        into projects (${keys}, version_id)
+        values (${values}, (select id from inserted_version))`);
+  }
+
+  async updateProject(
+    projectSlug: ProjectSlug,
+    changes: Partial<Omit<ProjectCore, "slug">>
+  ): Promise<void> {
+    const definedEntries = getEntriesWithDefinedValues(changes);
+    const setters = getUpdateAssigmentsSql(definedEntries);
+    if (definedEntries.length !== 0) {
+      await this.pool.query(sql`update projects
+                                set ${setters}
+                                where slug = ${projectSlug}`);
+    }
+  }
+
+  async deleteProject(projectSlug: ProjectSlug): Promise<void> {
+    await this.pool.query(sql`update projects
+                              set deleted_at = now()
+                              where slug = ${projectSlug}`);
+  }
+
+  writeFile(
+    projectSlug: ProjectSlug,
+    filePath: string,
+    contents: string | Uint8Array
+  ): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  updateDraftMetadata(
+    slug: string,
+    appMetadataChanges: Partial<DBInsertAppMetadataJSON>
+  ): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  writeProjectZip(
+    projectSlug: string,
+    zipContent: Uint8Array
+  ): Promise<Version> {
+    throw new Error("Method not implemented.");
+  }
+
+  async publishVersion(projectSlug: string): Promise<void> {
+    await this.pool.query(
+      sql`update versions v
+          set published_at=now()
+          where (published_at is null and v.id = (select version_id from projects p where slug = ${projectSlug}))`
+    );
+  }
+
   async getProject(projectSlug: string): Promise<Project> {
     return (await this.getProjects({ projectSlug }))[0]!;
   }
 
-  // TODO test
+  // TODO use and test
   async getDraftVersion(projectSlug: string): Promise<Version> {
     const selectVersionIdForProject = sql`select version_id from projects p where p.slug = ${projectSlug}`;
     const dbVersion: DBVersion & { app_metadata: DBAppMetadataJSON } =
@@ -79,6 +183,10 @@ export class BadgeHubDataPostgresAdapter implements BadgeHubDataPort {
   }
 
   getUser(userId: string): Promise<User> {
+    throw new Error("Method not implemented.");
+  }
+
+  updateUser(updatedUser: User): Promise<void> {
     throw new Error("Method not implemented.");
   }
 
