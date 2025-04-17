54f34767927d0cfcd37e763159247449ca73f5da
Francis Duvivier <francisduvivier@gmail.com>
2025-04-17 22:17:27 +0200

    BADGEHUB-52: add test for deleting draft file

---
+++ src/controllers/private-rest.spec.ts
@@ -0,0 +1,35 @@
+import { beforeEach, describe, expect, test } from "vitest";
+import request from "supertest";
+import express from "express";
+import { createExpressServer } from "@createExpressServer";
+import { isInDebugMode } from "@util/debug";
+
+describe(
+  "Authenticated API Routes",
+  () => {
+    let app: ReturnType<typeof express>;
+    beforeEach(() => {
+      app = createExpressServer(true);
+    });
+    test("CREATE/READ/DELETE /apps/{slug}/draft/files/{filePath}", async () => {
+      const postRes = await request(app)
+        .post("/api/v3/apps/codecraft/draft/files/test.txt")
+        .attach("file", Buffer.from("test file content"), "test.txt");
+      expect(postRes.statusCode.toString()).toMatch(/2\d\d/);
+      const getRes = await request(app).get(
+        "/api/v3/apps/codecraft/draft/files/test.txt"
+      );
+      expect(getRes.statusCode).toBe(200);
+      expect(getRes.text).toBe("test file content");
+      const deleteRes = await request(app).delete(
+        "/api/v3/apps/codecraft/draft/files/test.txt"
+      );
+      expect(deleteRes.statusCode.toString()).toMatch(/2\d\d/);
+      const getRes2 = await request(app).get(
+        "/api/v3/apps/codecraft/draft/files/test.txt"
+      );
+      expect(getRes2.statusCode).toBe(404);
+    });
+  },
+  { timeout: isInDebugMode() ? 3600_000 : undefined }
+);
