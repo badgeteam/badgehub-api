6611eb97fef46a3832b4a963d50287f58313d9fe
Edwin Martin <edwin@bitstorm.org>
2025-05-11 21:28:46 +0200

    Restore createExpressServer

--- src/createExpressServer.ts
+++ src/createExpressServer.ts
@@ -4,24 +4,32 @@ import { disableMutatingRest } from "@disableMutatingRest";
 import openapi from "@openapi";
 import { RegisterRoutes } from "@generated/routes";
 import { addTsoaValidationFailureLogging } from "@util/logging";
-import { NODE_ENV } from "@config";
 import multer from "multer";
-import { MAX_UPLOAD_FILE_SIZE_BYTES } from "@config";
+import { NODE_ENV, MAX_UPLOAD_FILE_SIZE_BYTES } from "@config";
+// TODO: enable when disableWriteWhenNotDev is available
+// import { disableWriteWhenNotDev } from "@disableWriteWhenNotDev";
+import { jwtErrorHandler } from "@auth/jwt-error";
+import rateLimit from "express-rate-limit";
 
 export const createExpressServer = (
   enableMutation = NODE_ENV === "development"
 ) => {
   const app = express();
-  app.use((req, res, next) => {
-    next(); // for inspection during development
+
+  const rateLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 500, // Limit each IP to 100 requests per windowMs
   });
-  const pino = pinoHttp();
+  app.use(rateLimiter);
+
   if (!enableMutation) {
     disableMutatingRest(app);
   }
 
   app.use(express.json());
   app.use(express.static("public"));
+
+  const pino = pinoHttp();
   app.use(pino);
 
   openapi(app);
@@ -30,6 +38,9 @@ export const createExpressServer = (
     multer: multer({ limits: { fileSize: MAX_UPLOAD_FILE_SIZE_BYTES } }),
   });
 
+  app.use(jwtErrorHandler);
+
   addTsoaValidationFailureLogging(app);
+
   return app;
 };
