3ac76f3075cc36cd36101d7c2e8c47cf4b4b58aa
Francis Duvivier <francisduvivier@gmail.com>
2025-05-29 20:38:54 +0200

    refactor: move api-docs to api-docs endpoint and fix authorization

--- src/serveApiDocs.ts
+++ src/serveApiDocs.ts
@@ -5,18 +5,24 @@ import { publicRestContracts } from "@shared/contracts/publicRestContracts";
 import { privateRestContracts } from "@shared/contracts/privateRestContracts";
 import { initContract } from "@ts-rest/core";
 import { createExpressEndpoints, initServer } from "@ts-rest/express";
-import { OpenAPIObject, OperationObject } from "openapi3-ts";
+import {
+  OpenAPIObject,
+  OperationObject,
+  ParameterObject,
+  ReferenceObject,
+} from "openapi3-ts";
 
 const c = initContract();
 const swaggerJsonContract = c.router({
   getSwaggerDoc: {
     method: "GET",
-    path: "/openapi/swagger.json",
+    path: "/api-docs/swagger.json",
     responses: {
       200: c.type<OpenAPIObject>(),
     },
   },
 });
+
 function withPrefix(prefix: string, paths: Record<string, any>) {
   const prefixedPaths: Record<string, any> = {};
   for (const [path, value] of Object.entries(paths)) {
@@ -30,16 +36,20 @@ const withTag = (operation: OperationObject, tag: string) => ({
   tags: [...(operation.tags ?? []), tag],
 });
 
+const isAuthorizationHeader = (p: ParameterObject | ReferenceObject) => {
+  return (
+    "in" in p &&
+    p.in === "header" &&
+    "name" in p &&
+    p.name?.toLowerCase() === "authorization"
+  );
+};
+
 const withSecurity = (operation: OperationObject) => ({
   ...operation,
-  security: [
-    {
-      BearerAuth: {
-        type: "http",
-        scheme: "bearer",
-      },
-    },
-  ],
+  // Remove authorization headers from parameters because they are handled by the security scheme and need to be filled in at the top
+  parameters: operation.parameters?.filter((p) => !isAuthorizationHeader(p)),
+  security: [{ bearer: ["private"] }],
 });
 
 const createSwaggerDoc = () => {
@@ -83,11 +93,21 @@ const createSwaggerDoc = () => {
       { url: "https://badgehub-api.p1m.nl/" },
       { url: "https://localhost:8081/" },
     ],
-  };
+    components: {
+      ...jsonSwagger.components,
+      securitySchemes: {
+        bearer: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+  } as const satisfies OpenAPIObject;
 };
 const swaggerDoc = createSwaggerDoc();
 
-export default function openapi(app: Express) {
+export default function serveApiDocs(app: Express) {
   const swaggerJsonRouter = initServer().router(swaggerJsonContract, {
     getSwaggerDoc: async () => ({
       status: 200,
@@ -96,10 +116,10 @@ export default function openapi(app: Express) {
   });
   createExpressEndpoints(swaggerJsonContract, swaggerJsonRouter, app);
   const options = {
-    swaggerOptions: { url: "/openapi/swagger.json" },
+    swaggerOptions: { url: "/api-docs/swagger.json" },
   };
   app.use(
-    "/openapi",
+    "/api-docs",
     swaggerUi.serveFiles(undefined, options),
     swaggerUi.setup(undefined, options)
   );
