e653f804b1f98a640676e87c20872213c4bddd65
Francis Duvivier <francis.duvivier@persgroep.net>
2025-05-31 21:24:27 +0200

    fix(frontend): fix tsconfig

--- packages/frontend/vitest.config.ts
+++ packages/frontend/vitest.config.ts
@@ -1,11 +1,26 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import type { CoverageOptions } from "vitest/node";
+import * as path from "node:path";
+
+const coverageConfig: CoverageOptions = {
+  reporter: ["text", "json-summary", "json"],
+  reportOnFailure: true,
+  provider: "v8",
+};
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [...react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@shared": path.resolve(__dirname, "../shared/src"),
+    },
+  },
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: ["./src/setupTests.ts"],
+    coverage: coverageConfig
   },
 });
