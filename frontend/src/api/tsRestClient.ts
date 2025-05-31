f26dc27f70a11871c462e26fd6d82322c354efa6
Francis Duvivier <francisduvivier@gmail.com>
2025-05-31 16:34:48 +0200

    WIP frontend

--- frontend/src/api/tsRestClient.ts
+++
@@ -1,8 +0,0 @@
-import { initClient } from "@ts-rest/core";
-import { publicRestContracts } from "../../../src/shared/contracts/publicRestContracts";
-
-export const tsRestClient = initClient(publicRestContracts, {
-  baseUrl: "/api", // Adjust if your API is served elsewhere
-  baseHeaders: {},
-  // credentials: 'include', // Uncomment if you need cookies
-});
---
+++ packages/frontend/src/api/tsRestClient.ts
@@ -0,0 +1,11 @@
+import { initClient } from "@ts-rest/core";
+// import { publicRestContracts } from "../../../src/shared/contracts/publicRestContracts";
+
+export const tsRestClient = initClient(
+  {},
+  {
+    baseUrl: "/api", // Adjust if your API is served elsewhere
+    baseHeaders: {},
+    // credentials: 'include', // Uncomment if you need cookies
+  }
+);
