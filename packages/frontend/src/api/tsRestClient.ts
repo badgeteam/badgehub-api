c50c2d6d90b63be70a609cf81c620352e8e51172
Francis Duvivier <francisduvivier@gmail.com>
2025-05-31 22:02:55 +0200

    fix(frontend): fix frontend requesting apps from backend

--- packages/frontend/src/api/tsRestClient.ts
+++ packages/frontend/src/api/tsRestClient.ts
@@ -1,11 +1,7 @@
 import { initClient } from "@ts-rest/core";
 import { publicRestContracts } from "@shared/contracts/publicRestContracts";
 
-export const tsRestClient = initClient(
-  publicRestContracts,
-  {
-    baseUrl: "/api", // Adjust if your API is served elsewhere
-    baseHeaders: {},
-    // credentials: 'include', // Uncomment if you need cookies
-  }
-);
+export const tsRestClient = initClient(publicRestContracts, {
+  baseUrl: "http://localhost:8081/api/v3", // Adjust if your API is served elsewhere
+  baseHeaders: {},
+});
