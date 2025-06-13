3aeb66f5297f7d8624488a1addcdb96cf0f6f7af
Francis Duvivier <francisduvivier@gmail.com>
2025-06-13 21:56:38 +0200

    fix(frontend): change Featured apps to Badge Projects

--- packages/frontend/src/components/AppsGrid/AppsGrid.tsx
+++ packages/frontend/src/components/AppsGrid/AppsGrid.tsx
@@ -11,7 +11,7 @@ const AppsGrid: React.FC<AppsGridProps> = ({ apps }) => {
     <>
       <section id="apps-grid">
         <h2 className="text-2xl font-semibold text-slate-100 mb-6">
-          Featured Applications
+          Badge Projects
         </h2>
         <div
           className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6"
