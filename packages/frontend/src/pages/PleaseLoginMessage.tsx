141193f7a003a7faf823a64419709d0e84c1353f
Francis Duvivier <francisduvivier@gmail.com>
2025-06-15 11:19:03 +0200

    feat(frontend): center please login text

--- packages/frontend/src/pages/PleaseLoginMessage.tsx
+++ packages/frontend/src/pages/PleaseLoginMessage.tsx
@@ -4,7 +4,7 @@ export const PleaseLoginMessage: React.FC<{ whatToSee: string }> = ({
   whatToSee,
 }) => (
   <>
-    <div className="items-center justify-center bg-gray-900 text-slate-200">
+    <div className="items-center justify-center bg-gray-900 text-slate-200 text-center">
       <p>Please log in to see {whatToSee}.</p>
     </div>
   </>
